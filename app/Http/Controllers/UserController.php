2e152c1b500ea4344c74db60c709ed5113bd6347
fokosun <okosunuzflorence@gmail.com>
2020-10-14 13:36:01 -0600

    New user validation rules

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace playlog\Http\Controllers;
+
+use playlog\Http\Requests\CreateUserRequest;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function store(CreateUserRequest $request)
+	{
+	}
+}

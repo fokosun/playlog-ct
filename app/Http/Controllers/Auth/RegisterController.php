4c2600eb74e8f2f3a25aafd38d55accf35f711d5
fokosun <okosunuzflorence@gmail.com>
2020-10-14 19:31:10 -0600

    override register method, framework validates differently from my specific requirement

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,10 +48,6 @@ class RegisterController extends Controller
 	 */
     public function register(CreateUserRequest $request)
 	{
-//		$this->validator($request->all())->validate();
-
-//		event(new Registered($user = $this->create($request->all())));
-
 		$user = $this->create($request->all());
 
 		$this->guard()->login($user);

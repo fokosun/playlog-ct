f15da9d554b769a07897b959a7005717ce083d75
fokosun <okosunuzflorence@gmail.com>
2020-10-14 19:28:25 -0600

    display home page

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Playlog\Http\Controllers;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
+}

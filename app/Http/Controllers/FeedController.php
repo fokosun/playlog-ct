e632dd8bb92c3e37893475993cd84a0a82396169
fokosun <okosunuzflorence@gmail.com>
2020-10-15 08:30:54 -0600

    add docblock comment

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -6,6 +6,11 @@ use Playlog\Comment;
 
 class FeedController extends Controller
 {
+	/**
+	 * Fetch all comments along with its relationships
+	 *
+	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application|\Illuminate\View\View
+	 */
     public function index()
 	{
 		$comments = Comment::with(['user', 'reactions'])

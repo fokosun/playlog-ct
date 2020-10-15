6dd316f40827382fc040913f7c658aac973a20f3
fokosun <okosunuzflorence@gmail.com>
2020-10-15 13:25:35 -0600

    make the root comment aware that there has been a new comment

--- app/Http/Controllers/CommentReactionController.php
+++ app/Http/Controllers/CommentReactionController.php
@@ -2,7 +2,9 @@
 
 namespace Playlog\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Playlog\Comment;
 use Playlog\CommentReaction;
 
 class CommentReactionController extends Controller
@@ -23,6 +25,10 @@ class CommentReactionController extends Controller
 		if (! $reaction->save()) {
 			return redirect()->back()->withErrors('There was ab error processing this request. Please try again');
 		}
+
+		$root_comment = Comment::find($comment_id);
+
+		$root_comment->update(['updated_at' => Carbon::now()->toDateTimeString()]);
 
 		return redirect()->back();
 	}

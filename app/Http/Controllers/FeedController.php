9067f7425c650e673954bfeeee1da2d73b701188
fokosun <okosunuzflorence@gmail.com>
2020-10-15 08:29:51 -0600

    paginate comments collection

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -8,9 +8,9 @@ class FeedController extends Controller
 {
     public function index()
 	{
-		$comments = Comment::with(['user', 'reactions'])->orderBy('updated_at', 'desc')->get();
-//		$f = $comments->first();
-//		dd($f->reactions->first()->posted_on);
+		$comments = Comment::with(['user', 'reactions'])
+			->orderBy('updated_at', 'desc')
+			->paginate('5');
 
 		return view('feed', compact('comments'));
 	}

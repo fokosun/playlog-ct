247c38d5393c002fbfbe8ec6d3080e0d3a975974
fokosun <okosunuzflorence@gmail.com>
2020-10-15 17:44:42 -0600

    check if file exists then delete

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -63,7 +63,7 @@ class CommentController extends Controller
 			return redirect()->back()->withErrors(['error' => 'Forbidden. You are not authorized to perform this action.']);
 		}
 
-		if ($comment->photo_url || Storage::disk('public')->get($comment->photo_url)) {
+		if ($comment->photo_url || Storage::disk('public')->exists($comment->photo_url)) {
 			Storage::disk('public')->delete($comment->photo_url); //delete the image to declutter storage
 		}
 

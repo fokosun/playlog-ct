3c5129aa303ccd7bd9b57f0f8f952f42039dddf8
fokosun <okosunuzflorence@gmail.com>
2020-10-16 15:29:26 -0600

    quick fix

--- resources/views/components/comments/reaction.blade.php
+++ resources/views/components/comments/reaction.blade.php
@@ -1,7 +1,9 @@
 <form action="{{ route('reaction.new', [\Illuminate\Support\Facades\Auth::user()->id, $comment->id]) }}" method="POST">
     <input type="hidden" id="token" name="_token" value="{{ csrf_token() }}">
     <div>
-        <input type="text" name="new_comment" id="new_comment" placeholder="write a comment ..." class="main-content" required>
-        <input type="submit" value="Post" class="submit-content">
+        <input type="text" name="comment" id="{{ $comment->id }}" placeholder="write a comment ..." class="main-content" required />
+        <input type="hidden" name="author_id" value="{{ \Illuminate\Support\Facades\Auth::user()->id }}" />
+        <input type="hidden" name="comment_id" value="{{ $comment->id }}" />
+        <input type="submit" value="Post" class="submit-content" />
     </div>
 </form>

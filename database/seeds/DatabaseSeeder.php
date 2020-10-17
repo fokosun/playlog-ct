44cb82cdc0588343b77431a958cd46ee7b06e8ad
fokosun <okosunuzflorence@gmail.com>
2020-10-17 13:59:56 -0600

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use Playlog\User;
+use Playlog\Comment;
+use Playlog\CommentReaction;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+    	$min = 1;
+    	$max = 200;
+
+        $users = factory(User::class, $max)->create();
+
+        foreach ($users as $user) {
+        	//15 each
+			factory(Comment::class)->create([
+				'author_id' => $user->getKey()
+			]);
+		}
+
+        $comments = Comment::all();
+
+        foreach ($comments as $comment) {
+        	factory(CommentReaction::class)->create([
+        		'author_id' => rand($min, $max),
+				'comment_id' => $comment->id
+			]);
+		}
     }
 }
